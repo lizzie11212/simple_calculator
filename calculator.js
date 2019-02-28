0c6f1b89df4811a8ab7ea392e0595ba11a76c6d8
Hsiao Li Lin <hsiaolilin@Hsiaos-MacBook-Air.local>
2019-02-28 13:10:03 -0500

    add calculator constructor in calculator.js

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function Calculator(){
+	this.total =0 ;
+}
+
+Calculator.prototype.add = function(number){
+	return this.total += number;
+}
+
+Calculator.prototype.substract = function(number){
+	return this.total -= number;
+}
+
+Calculator.prototype.multiply = function(number){
+	return this.total *= number;
+}
+
+Calculator.prototype.divide = function(number){
+	if (number === 0) {
+		throw new Error('Cannot divide by zero');
+	}
+	
+	return this.total /= number;
+}
