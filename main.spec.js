41b7aab0e379efb779bcc9a16e0f8e7e79dbaa9c
Hsiao Li Lin <hsiaolilin@Hsiaos-MacBook-Air.local>
2019-03-03 04:38:08 -0500

    use beforeAll, afterAll to update main.spec.js

--- main.spec.js
+++ main.spec.js
@@ -1,9 +1,34 @@
 describe('main.js', function(){
 	describe('calculate()', function(){
-		it('validates expression');
+		xit('validates expression');
+		xit('calls add');
+		xit('calls substract');
+		xit('calls multiply');
+		xit('calls divide');
+		xit('validates operation');
+		xit('calls updateResult');
 	});
 
 	describe('updateResult()',function(){
+		let element;
+		beforeAll(function(){
+			// Excuted ONCE before all specs are executed.
+			element = document.createElement('div');
+			element.setAttribute('id','result');
+			document.body.appendChild(element);
+		});
 
+		afterAll(function(){
+			// Excuted ONCE after all specs are executed.
+			const element = document.getElementById('result');
+			document.body.removeChild(element);
+
+		})
+
+		it('adds result to DOM element', function(){
+			updateResult('5');
+
+			expect(element.innerText).toBe('5');
+		});
 	});
 });
